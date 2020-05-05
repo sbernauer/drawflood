eeecc41a4b5258dafa40ded5ecf4e57d53bdcd2a
Sebastian Bernauer <sebastian.bernauer@1und1.de>
2020-05-05 19:39:31 +0200

    Added listen address

--- drawflood.c
+++ drawflood.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <getopt.h>
 
 #include "drawflood.h"
 
+#define LISTEN_ADDRESS_DEFAULT "::"
+
 int main(int argc, char *argv[]) {
 	printf("Starting drawflood\n");
+	char* listen_address = LISTEN_ADDRESS_DEFAULT;
 
-	net_listen();
+	char opt;
+	while((opt = getopt(argc, argv, "l:")) != -1) {
+		switch(opt) {
+			case('l'):
+				listen_address = optarg;
+				break;
+		}
+	}
+
+
+	net_listen(listen_address);
 }
 
-void net_listen() {
-	printf("net_listen\n");
+void net_listen(char* listen_address) {
+	printf("Listening on %s\n", listen_address);
 }
